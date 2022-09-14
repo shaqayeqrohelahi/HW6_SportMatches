831a4caff7253b10645d7c2d6dd074f973eacaa8
Shaqayeq Rohelahi <rohelahi.shaqayeq@gmail.com>
2022-09-15 03:21:05 +0430

    add repo and service classes/complete some parts in Main class

---
+++ src/main/java/model/FootballTeam.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Team1 {
+    private int id;
+    private String name;
+    private int winNumber;
+    private int loseNumber;
+    private int score;
+    private int matchNumber;
+}
